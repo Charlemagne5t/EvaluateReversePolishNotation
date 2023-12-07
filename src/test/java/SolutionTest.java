17cb608d1ef6f9a6c0bfb0d11844bb1fc6b3b8a0
alek <wait82985@gmail.com>
2023-12-07 11:33:06 +0600

    initial commit. 3 test cases

---
+++ src/test/java/SolutionTest.java
@@ -0,0 +1,29 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SolutionTest {
+    @Test
+    public void evalRPNTest1(){
+        String[] tokens = {"2","1","+","3","*"};
+        int expected = 9;
+        int actual = new Solution().evalRPN(tokens);
+
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void evalRPNTest2(){
+        String[] tokens = {"4","13","5","/","+"};
+        int expected = 6;
+        int actual = new Solution().evalRPN(tokens);
+
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void evalRPNTest3(){
+        String[] tokens = {"10","6","9","3","+","-11","*","/","*","17","+","5","+"};
+        int expected = 22;
+        int actual = new Solution().evalRPN(tokens);
+
+        Assert.assertEquals(expected, actual);
+    }
+}
